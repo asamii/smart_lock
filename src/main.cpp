1aedf4914bee4229b42bcd08ee4819dd3cf68426
asamii <handwhistling.3142@gmail.com>
2022-06-05 13:59:29 +0900

    Motor operation test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+
+Servo servo;
+int const SERVO_PIN = 15;
 
 void setup() {
   // put your setup code here, to run once:
+  servo.attach(SERVO_PIN);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  servo.write(0);
+  delay(1000);
+  servo.write(90);
+  delay(1000);
+  servo.write(180);
+  delay(1000);
 }
